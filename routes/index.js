8dad6d36a5ee330698d56b6be35d8be682acd924
Jochem Dijkdrent <dijk0146@hz.nl>
2022-09-22 08:51:09 +0200

    node_modules?

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,42 @@ router.get('/', (req, res, next) => {
   res.json('hi');
 });
 
+router.get('/onepiece', (req, res, next) => {
+  res.json('The One Piece is real!!!!!');
+});
+
+function myLogger(req, res, next) {
+  console.log(req.query);
+  next()
+}
+
+router.get('/appointments', myLogger, (req, res, next) => {
+  res.json({
+    'title': 'Its real',
+    'url': 'url-to-itself',
+    'query': req.query,
+    'appointments': [
+      'http://my-domain/appointments/1'
+    ],
+  })
+  res.json('Wednesday: One Piece film Red');
+});
+
+router.post('/appointments', myLogger, (req, res, next) => {
+  res.json('Wednesday: One Piece film Red');
+});
+
+router.options('/appointments', myLogger, (req, res, next) => {
+  res.header({
+    allow: "PUT, GET, OPTIONS"
+  });
+  res.json('Wednesday: One Piece film Red');
+});
+
+router.get('/class', myLogger, (req, res, next) => {
+
+});
+
+
+
 export default router;
